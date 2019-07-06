2c3f9ad69f666cf242cfb8b0b1c933328e1b46ef
Andrey Obzhigalov <obzhigalov.andrew@gmail.com>
2019-07-06 19:44:12 +0300

    Сделал меню, поправил скиллы

--- src/scripts/fs-menu.js
+++ src/scripts/fs-menu.js
@@ -1,11 +1,33 @@
 function menu() {
   const burger = document.querySelector('.burger');
   const fsMenu = document.querySelector('.fs-menu');
-  const burgerActive = document.querySelector('.burger--active');
 
   burger.addEventListener('click', function() {
     burger.classList.toggle('burger--active');
     fsMenu.classList.toggle('fs-menu--active');
+  });
+
+  fsMenu.addEventListener('click', ()=> {
+    
+    const target = event.target;
+
+    console.log(target)
+
+    if(target.classList.contains('nav__list-link')) {
+      burger.classList.toggle('burger--active');
+      fsMenu.classList.toggle('fs-menu--active');
+    }
+
+    if(target.classList.contains('social__svg')) {
+      burger.classList.toggle('burger--active');
+      fsMenu.classList.toggle('fs-menu--active');
+    }
+
+    if(target.hasAttribute('xlink:href')) {
+      burger.classList.toggle('burger--active');
+      fsMenu.classList.toggle('fs-menu--active');
+    }
+
   })
   
 };
