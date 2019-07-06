74038d0605026cede7f0103bb9c4f638317036f1
Andrey Obzhigalov <obzhigalov.andrew@gmail.com>
2019-07-06 20:02:07 +0300

    Test

--- src/scripts/fs-menu.js
+++ src/scripts/fs-menu.js
@@ -1,10 +1,12 @@
 function menu() {
   const burger = document.querySelector('.burger');
   const fsMenu = document.querySelector('.fs-menu');
+  const wrapper = document.querySelector('.wrapper');
 
   burger.addEventListener('click', function() {
     burger.classList.toggle('burger--active');
     fsMenu.classList.toggle('fs-menu--active');
+    wrapper.classList.toggle('wrapper--fs-menu');
   });
 
   fsMenu.addEventListener('click', ()=> {
@@ -16,16 +18,19 @@ function menu() {
     if(target.classList.contains('nav__list-link')) {
       burger.classList.toggle('burger--active');
       fsMenu.classList.toggle('fs-menu--active');
+      wrapper.classList.toggle('wrapper--fs-menu');
     }
 
     if(target.classList.contains('social__svg')) {
       burger.classList.toggle('burger--active');
       fsMenu.classList.toggle('fs-menu--active');
+      wrapper.classList.toggle('wrapper--fs-menu');
     }
 
     if(target.hasAttribute('xlink:href')) {
       burger.classList.toggle('burger--active');
       fsMenu.classList.toggle('fs-menu--active');
+      wrapper.classList.toggle('wrapper--fs-menu');
     }
 
   })
